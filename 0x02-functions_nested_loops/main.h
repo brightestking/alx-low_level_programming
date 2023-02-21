779791bcfbe5c89e2c29f27ba53f156a6daa7926
Ajuzie <brightajuzie8@gmail.com>
2023-02-21 13:53:12 -0800

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <stdio.h>
 
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
+void print_message(void)
+{
+	printf("_putchar\n");
+	return;
+}
 
-#endif
+void print_alphabet()
+{
+	char min = 'a';
+	while (min <= 'a')
+	{
+		printf("%c", min);
+		min++;
+	}
+
+}
